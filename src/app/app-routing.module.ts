13142466a1d0310c104b2ab7557c6e700fb620c9
ScarboroughCoral <3249977074@qq.com>
2020-08-09 13:35:07 +0800

    ADD: threejs训练场&NOT FOUND

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,29 @@
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
 import { WelcomeComponent } from './modules/welcome/welcome.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'welcome',
     component: WelcomeComponent
   },
   {
     path: 'home',
     loadChildren: () => import("./modules/home/home.module").then(m => m.HomeModule)
+  },
+  {
+    path: 'threejs-training',
+    loadChildren: () => import("./modules/threejs-training/threejs-training.module").then(m => m.ThreejsTrainingModule)
+  },
+  {
+    path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
